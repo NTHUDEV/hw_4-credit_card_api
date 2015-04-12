26997b5caa67d3e53d7aa623ec2bb515e5d74754
csrordzhn <c_man182@yahoo.com>
2015-04-13 00:14:39 +0800

    2015-04-12 modified Readme to include api link and app.rb.

--- app.rb
+++ app.rb
@@ -10,7 +10,6 @@ end
 get '/api/v1/credit_card/validate' do
   creditcard = CreditCard.new(params[:card_number], nil, nil, nil)
   {card: params[:card_number].to_s, validated: creditcard.validate_checksum}.to_json
-
 end
 
 end
